8fd0c59762fb35f1898f69fc3b7dc03a079c6d1b
guokaigdg <guokaigdg@gmail.com>
2023-06-08 17:34:09 +0800

    feat: 增加 HashRouter 路由

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {BrowserRouter} from 'react-router-dom';
-import {basename} from '@/utils/variable';
+import {HashRouter} from 'react-router-dom';
+// import {basename} from '@/utils/variable';
 import {StoresProvider, stores} from '@/store';
 import '@/assets/icons/index';
 import App from './App';
@@ -10,9 +10,14 @@ const rootElement = document.getElementById('root');
 const root = ReactDOM.createRoot(rootElement as HTMLDivElement);
 
 root.render(
-    <BrowserRouter basename={basename}>
+    // <BrowserRouter basename={basename}>
+    //     <StoresProvider value={stores}>
+    //         <App />
+    //     </StoresProvider>
+    // </BrowserRouter>
+    <HashRouter>
         <StoresProvider value={stores}>
             <App />
         </StoresProvider>
-    </BrowserRouter>
+    </HashRouter>
 );
