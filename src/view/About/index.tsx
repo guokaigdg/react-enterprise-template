7b11dd89a627a78fa03dedaed127dc412271f4c7
guokaigdg <guokaigdg@gmail.com>
2024-05-08 15:16:20 +0800

    feat: ⚡增加FSM有限状态机模式

--- src/view/About/index.tsx
+++ src/view/About/index.tsx
@@ -1,22 +1,61 @@
-import React from 'react';
-import {observer} from 'mobx-react-lite';
-// import {useStores} from '@/store';
-// import SvgIcon from '@/components/SvgIcon';
+import React, {useState, useEffect} from 'react';
+import {observer, useLocalObservable} from 'mobx-react-lite';
+import {runInAction} from 'mobx';
+import {GetResponsePokemonData} from '@/api/home-two/types/home-two';
 import {GithubLogo} from '@phosphor-icons/react';
+import {fetchPokemon} from '@/api/home-two';
 import './index.less';
 
+interface StoreType {
+    pokemon: string;
+    getFetchPokemo: () => void;
+}
+// FSM模式: 有限状态机
+// 有限状态机（Finite-state machine,FSM），又称有限状态自动机，简称状态机，表示有限个状态以及在这些状态之间的转移和动作等行为的数学模型
+type DataStateType = 'LOADING' | 'SUCCESS' | 'ERROR';
+
 const About = () => {
-    // const {globalStore} = useStores();
-    // const {count, name} = globalStore;
+    const [dataState, setDataState] = useState<DataStateType>('LOADING');
+    const PokemoStore = useLocalObservable<StoreType>(() => ({
+        pokemon: '',
+        async getFetchPokemo() {
+            try {
+                const res: GetResponsePokemonData = await fetchPokemon({
+                    limit: 500
+                });
+                const {results} = res.data;
+                if (results) {
+                    setDataState('SUCCESS');
+                    runInAction(() => {
+                        this.pokemon = 'Pokemon';
+                    });
+                } else {
+                    setDataState('ERROR');
+                    runInAction(() => {
+                        this.pokemon = 'ERROR';
+                    });
+                }
+            } catch (err) {
+                setDataState('ERROR');
+            }
+        }
+    }));
+    const {getFetchPokemo, pokemon} = PokemoStore;
+
+    useEffect(() => {
+        getFetchPokemo();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
     return (
         <div className='about-root'>
             <a href='https://github.com/guokaigdg/react-enterprise-template'>
                 <GithubLogo size={68} color='#f9f4da' />
             </a>
             <p>Hello About</p>
-            {/* <SvgIcon svgName='apple' svgClass='icon-apple' /> */}
-            {/* <p>{name}</p> */}
-            {/* <p>{count}</p> */}
+            {dataState === 'LOADING' && <div>LOADING</div>}
+            {dataState === 'SUCCESS' && <div>SUCCESS {pokemon}</div>}
+            {dataState === 'ERROR' && <div>ERROR {pokemon}</div>}
         </div>
     );
 };
